c999b558d31e3900b262e7f0d4fb64a0939b8869
sanadnassar <194846666+sanadnassar@users.noreply.github.com>
2025-11-17 17:03:46 -0500

    Sketch A Elf Project
    Finished The Project

---
+++ script.js
@@ -0,0 +1,20 @@
+const main = document.querySelector("#main");
+const button = document.querySelector("button")
+
+
+for (let i = 0; i < 256; i++) {
+    main.appendChild(document.createElement("div"));
+}
+
+document.addEventListener(("mouseover"), (e)=>{
+    if (e.target.parentNode === main) {
+        e.target.style.backgroundColor = "#000000ff";
+    }
+})
+
+document.addEventListener("click", (e)=>{
+    const child = main.children;
+    for (let i = 0; i < 256; i++) {
+        child[i].style.backgroundColor = "#ff0000";
+}
+})
